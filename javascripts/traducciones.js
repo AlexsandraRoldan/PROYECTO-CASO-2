5ac1d7f36f0eda5801331d7028bbaf0fdfd533da
AlexsandraRoldan <alexsandraroldan16@gmail.com>
2025-03-13 16:05:39 -0500

    Funcionalidad de traducción entre inglés y español (implementada)

--- javascripts/traducciones.js
+++
@@ -1,9 +0,0 @@
-var check=document.querySelector(".check"); 
-check.addEventListener('click', idioma); 
-function idioma(){ 
-let id=check.checked; 
-if (id==true) { 
-location.href="../pagina_1_ingles.html"; 
-}else{ 
-location.href="../pagina_1_es.html"; 
-}}
